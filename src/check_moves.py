441209872e92f25fb75ad1d878bd8c3acc797193
Flemis <coderflemis@gmail.com>
2022-06-14 17:13:24 -0300

    fix bug on check_block fn and all fn to return early

--- src/check_moves.py
+++ src/check_moves.py
@@ -1,42 +1,35 @@
 def check_hor_ver(matrix, target_row, target_col, value, line):
-    is_valid_move = True
-    motive = []
     for i in range(9):
-        # Check horizontally
+        # Not check for the value on same col
         if i != target_col:
+            # Check horizontally
             if matrix[target_row][i] == value:
-                is_valid_move = False
-                motive = ["bad row move", target_row, i, value, line]
-                break
-        # Check vertically
+                return False, ["bad row move", target_row, i, value, line]
+        # Not check for value on same row
         if i != target_row:
-            if matrix[i][target_col] == value:
-                is_valid_move = False
-                motive = ["bad column move", i, target_col, value, line]
-                break
 
-    return is_valid_move, motive
+            # Check vertically
+            if matrix[i][target_col] == value:
+                return False, ["bad column move", i, target_col, value, line]
+
+    return True, []
 
 
 def check_block(matrix, target_row, target_col, value, line):
-    is_valid_move = True
-    motive = []
     row_block = int(target_row / 3)
     col_block = int(target_col / 3)
 
     for i in range(row_block * 3, row_block * 3 + 3):
-        for j in range(col_block * 3, col_block * 3 + 3):
-            if matrix[i][j] == value:
-                is_valid_move = False
-                motive = ["bad block move", i, j, value, line]
-                break
+        if i != target_row:
+            for j in range(col_block * 3, col_block * 3 + 3):
+                if j != target_col:
+                    if matrix[i][j] == value:
+                        return False, ["bad block move", i, j, value, line]
 
-    return is_valid_move, motive
+    return True, []
 
 
 def check_all_moves(matrix, target_row, target_col, value, line):
-    is_valid_move = True
-    motive = []
     (is_good_hor_ver, mot_hor) = check_hor_ver(
         matrix, target_row, target_col, value, line
     )
@@ -44,28 +37,19 @@ def check_all_moves(matrix, target_row, target_col, value, line):
         matrix, target_row, target_col, value, line
     )
     if is_good_hor_ver is False:
-        motive = mot_hor
-        is_valid_move = False
+        return False, mot_hor
     elif is_good_block is False:
-        motive = mot_block
-        is_valid_move = False
+        return False, mot_block
 
-    return is_valid_move, motive
+    return True, []
 
 
 def check_hint(row, column, value, line):
-    is_valid_hint = True
-    motive = []
-
     if row is False:
-        motive = ["bad row hint", row, column, value, line]
-        is_valid_hint = False
-
+        return False, ["bad row hint", row, column, value, line]
     elif column is False:
-        motive = ["bad column hint", row, column, value, line]
-        is_valid_hint = False
+        return False, ["bad column hint", row, column, value, line]
     elif value is False:
-        motive = ["bad value hint", row, column, value, line]
-        is_valid_hint = False
+        return False, ["bad value hint", row, column, value, line]
 
-    return is_valid_hint, motive
+    return True, []
