61f1641b249fcfafacbe3a1cbf1a2397621f72aa
Luiz Gustavo <gustavo1204.gc@gmail.com>
2022-06-17 19:04:57 -0300

    Refactor generate_grid in order to avoid the duplicated grid_cell value bug

--- src/helper_functions.py
+++ src/helper_functions.py
@@ -1,6 +1,8 @@
-import copy
-
-
-def grid_generator(lines, columns, is_hint=True):
-    grid_cell = {"value": " ", "is_hint": is_hint}
-    return [[copy.deepcopy(grid_cell) for _ in range(columns)] for _ in range(lines)]
+# Generate a grid `lines` X `columns`, each grid_cell
+# is a dictionary containing `value` and `is_hint`.
+def grid_generator(lines, columns):
+    return [
+        [
+            {"value": " ", "is_hint": False} for _ in range(columns)
+        ] for _ in range(lines)
+    ]
