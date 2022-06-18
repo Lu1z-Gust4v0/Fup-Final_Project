ffc0716a2d5e72315ed7b9ab03eda0901ce77cf1
Luiz Gustavo <gustavo1204.gc@gmail.com>
2022-06-18 00:34:49 -0300

    Fix bug introduced when 'parse_input' was modified.

--- src/read_files.py
+++ src/read_files.py
@@ -1,28 +1,41 @@
 from .args_parser import parse_input, raw_input
-from .checkers import check_all_moves, check_input
+from .checkers import check_all_moves
 from .helper_functions import grid_generator
 
 
-def generate_plays(play_file, initial_grid):
-    played_grid = initial_grid
-    with open(play_file) as file:
+def populate_moves(moves_file, initial_grid):
+    game_grid = initial_grid
+
+    with open(moves_file) as file:
         for line in file:
+            
+            parsed_input = parse_input(line)
+            raw_output = raw_input(line)
 
-            # TODO: Fix generate_play functions, since it broke due to the changes 
-            # in `parse_input` function
-            row, column, value = parse_input(line)
-            raw_row, raw_column, raw_value = raw_input(line)
+            # raw_output is a falsy value when the format is invalid
+            if not raw_output:
+                print("Formato invalido de jogada.")
+                continue
 
-            is_valid_hint, _ = check_input(row, column, value)
-            is_valid_move, _ = check_all_moves(played_grid, row, column, value)
-            # TODO: Perguntar ao miguel se mesmo uma jogada inválida no modo batch
-            # Se ela deve entrar no grid mesmo assim
-            if is_valid_hint and is_valid_move:
-                played_grid[row][column] = value
-            else:
+            raw_row, raw_column, raw_value = raw_output
+            row, column, value = parsed_input
+
+            # parsed_input is a falsy value when, at least, one of the 
+            # values provided by the user is invalid.  
+            if not parsed_input:
                 print(f"A jogada ({raw_column}{raw_row}) = {raw_value} eh invalida!")
+                continue
 
-    return played_grid
+            valid_move, _ = check_all_moves(game_grid, row, column, value)
+
+            # Check if the move is against the game rules.
+            if not valid_move:
+                print(f"A jogada ({raw_column}{raw_row}) = {raw_value} eh invalida!")
+                continue
+            
+            game_grid[row][column]["value"] = value
+            
+    return game_grid
 
 
 # Receives a PathBuf to config_file and return a grid filled with valid hints,
@@ -45,7 +58,7 @@ def populate_grid(config_file):
             
             row, column, value = parsed_input
             
-            valid_move, motive = check_all_moves(initial_grid, row, column, value)
+            valid_move, _ = check_all_moves(initial_grid, row, column, value)
 
             # Check if the hint it is against the game rules.
             if not valid_move:
