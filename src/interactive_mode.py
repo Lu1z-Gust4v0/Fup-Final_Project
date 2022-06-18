e9261a560966a452273677545781d280c7ff05c5
Luiz Gustavo <gustavo1204.gc@gmail.com>
2022-06-18 00:35:53 -0300

    Rename argument of 'iteractive_mode'

--- src/interactive_mode.py
+++ src/interactive_mode.py
@@ -3,13 +3,13 @@ from .checkers import check_all_moves, is_cell_hint, check_grid_completed
 from .frontend import display_grid
 
 
-def interactive_mode(initial_grid):
+def interactive_mode(game_grid):
 
     game_finished = False
 
     while not game_finished:
 
-        display_grid(initial_grid)
+        display_grid(game_grid)
 
         user_input = input("Faca uma jogada: ")
 
@@ -23,26 +23,26 @@ def interactive_mode(initial_grid):
 
             row, column, value = parsed_input
 
-            valid_move, motive = check_all_moves(initial_grid, row, column, value)
+            valid_move, _ = check_all_moves(game_grid, row, column, value)
 
             raw_row, raw_column, raw_value = raw_input(user_input)
             
             if not valid_move:
-                print(f"Jogada ({raw_column},{raw_row}:{raw_value}) Invalida, tente novamente\n")
+                print(f"Jogada ({raw_column},{raw_row}:{raw_value}) Invalida, tente novamente.")
                 continue
             
-            initial_grid[row][column]["value"] = value
+            game_grid[row][column]["value"] = value
 
         elif len(parsed_input) == 2:
 
             row, column = parsed_input
 
-            if is_cell_hint(initial_grid, row, column):
-                print("Voce nao pode deletar uma dica, tente novamente")
+            if is_cell_hint(game_grid, row, column):
+                print("Voce nao pode deletar uma dica, tente novamente.")
                 continue
 
-            initial_grid[row][column]["value"] = " "
+            game_grid[row][column]["value"] = " "
 
-        game_finished = check_grid_completed(initial_grid)
+        game_finished = check_grid_completed(game_grid)
     
-    print("Parabens, voce venceu")
+    print("Parabens, voce venceu!")
