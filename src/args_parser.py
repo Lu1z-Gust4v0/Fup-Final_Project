40791ca22ed22ee52e0243350f6e303005ebf742
Luiz Gustavo <gustavo1204.gc@gmail.com>
2022-06-17 21:14:11 -0300

    Refactor 'parse_input' function
    - 'parse_input' is now able to handle both insert and delete commands
    provided by the user.
    - It can be reused in other parts of the program.
    - It returns already parsed row, column, and value.

--- src/args_parser.py
+++ src/args_parser.py
@@ -9,7 +9,7 @@ def parse_column(column):
     if column in TEMPLATE_STRING:
         return TEMPLATE_STRING.index(column)
 
-    return False
+    return None
 
 
 # Search for a row in [1,9], if not found return False
@@ -19,7 +19,7 @@ def parse_row(row):
     if row >= 1 and row <= 9:
         return row - 1
 
-    return False
+    return None
 
 
 # Search for a value in [1,9], if not found return False
@@ -28,21 +28,7 @@ def parse_value(value):
     if value >= 1 and value <= 9:
         return value
 
-    return False
-
-
-# Receive a line and transform that line, if valid, in a parsed_input
-def parse_input(input):
-    splited_line = input.split(":")
-
-    column, row = (splited_line[0]).split(",")
-    value = splited_line[1]
-
-    parsed_row = parse_row(row)
-    parsed_column = parse_column(column)
-    parsed_value = parse_value(value)
-
-    return parsed_row, parsed_column, parsed_value
+    return None
 
 
 def raw_input(input):
@@ -53,12 +39,15 @@ def raw_input(input):
 
     input_grouped = input_pattern.search(new_input)
 
-    if input_grouped is None:
-        return False, ['no "," or ";"'], False
+    # If the input does not match it returns a None list
+    if not input_grouped:
+        return False
+    # if input_grouped is None:
+    #     return False, ['no "," or ";"'], False
 
-    column, row, value = input_grouped.groups()  # pyright: ignore
-
-    return row, column, value
+    raw_column, raw_row, raw_value = input_grouped.groups()  # pyright: ignore
+    
+    return [raw_row, raw_column, raw_value]
 
 
 # Search for a delete pattern, if not found return none
@@ -72,10 +61,56 @@ def search_for_delete_cmd(input):
     is_matched = input_pattern.search(new_input)
 
     # If not found, the regex return None
-    if is_matched is not None:
-        # We destructure the groups tuple in a string with "D", that is discarted
-        # column and row value
-        _, column, row = is_matched.groups()  # pyright: ignore
-        return _, column, row
+    if not is_matched:
+        return False
+        
+    # We destructure the groups tuple in a string with "D", that is discarted
+    # column and row value
+    _, column, row = is_matched.groups()  # pyright: ignore
+    return [column, row]
 
-    return None
+
+# This function receives an input and check if it is a valid input / command. 
+def parse_input(input):
+
+    raw_move_input = raw_input(input)
+    raw_del_input = search_for_delete_cmd(input) 
+
+    # Debug
+    # print(raw_move_input)
+    # print(raw_del_input)
+
+    if raw_move_input:
+
+        row, column, value = raw_move_input
+
+        parsed_row = parse_row(row)
+        parsed_column = parse_column(column)
+        parsed_value = parse_value(value)
+
+        # Debug
+        # print(parsed_row)
+        # print(parsed_column)
+        # print(parsed_value)
+
+        # Check each parsed value, if at least one of them is None, return False.
+        if not all(not i == None for i in [parsed_row, parsed_column, parsed_value]):
+            return False
+
+        return [parsed_row, parsed_column, parsed_value]
+
+    elif raw_del_input:
+
+        column, row = raw_del_input
+
+        parsed_row = parse_row(row)
+        parsed_column = parse_column(column)
+
+        # Check each parsed value, if at least one of them is None, return False.        
+        if not all(not i == None for i in [parsed_row, parsed_column]):
+            return False
+
+        return [parsed_row, parsed_column]
+    
+    else: 
+        return False
