8760bdde7f0e79f635768b7af9f67cd50df38970
Luiz Gustavo <gustavo1204.gc@gmail.com>
2022-06-18 00:14:37 -0300

    Rewrite error messages

--- sudoku.py
+++ sudoku.py
@@ -5,7 +5,7 @@ from src.interactive_mode import interactive_mode
 
 
 def main():
-    if len(sys.argv) < 2 or len(sys.argv) > 3:
+    if len(sys.argv) != 2 and len(sys.argv) != 3:
         print(
             f"""
 A quantidade de "{len(sys.argv) -1}" argumentos não é esperada pelo Sudoku
@@ -14,27 +14,24 @@ Por favor, insira ou apenas o arquivo de "cfg" ou o de "cfg" E o de "play"
         )
         exit()
 
-    (grid, hint_counter, wrong_hints) = populate_grid(sys.argv[1])
-    # display_grid(grid)
+    (grid, hint_counter, invalid_hint) = populate_grid(sys.argv[1])
+
+    if invalid_hint: 
+        print(
+            f"""
+Arquivo de dicas invalido.
+Dica invalida: {invalid_hint}
+            """
+        )
+        exit()
 
     if hint_counter < 1 or hint_counter > 80:
         print(
             f"""
-A quantidade de entradas {hint_counter} não está no intervalo [1,80]
-Por favor, insira entradas válidas
-                """
-        )
-        exit()
-
-    if len(wrong_hints) > 0:
-        print(
+A quantidade minima de dicas é 1 e a máxima é 80.
+Você inseriu {hint_counter} dicas. 
             """
-Encontramos algumas entradas erradas no arquivo de configuração
-Por favor, conserte as seguintes entradas:
-                """
         )
-        for hint in wrong_hints:
-            print(f"Linha: {hint[4]}, Motivo: {hint[0]}")
         exit()
 
     if len(sys.argv) == 2:
